0b2f61f4e239473da50025d67007060e47a36940
Gerasev Kirill <gerasev.kirill@gmail.com>
2017-01-03 10:09:53 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
-from setuptools import setup, find_packages
-import django_gcs
+import os
+from setuptools import setup
 
-setup_options = {
-        'name': 'django-gcloud-storage',
-        'version': django_gcs.__version__,
-        'install_requires': [
-            'Django>=1.10',
-            'google-cloud==0.22.0'
-        ],
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
-        'author': 'Colin Su',
-        'author_email': 'littleq0903@gmail.com',
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-        'license': 'MIT',
-        'description': open('./README.md').read(),
-        'url': 'https://github.com/gerasev-kirill/django-gcloud-storage',
-
-        'classifiers': [
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Framework :: Django',
-        ]
-}
-
-setup(**setup_options)
+setup(
+    name='django-gcloud-storage',
+    version='0.22.0',
+    packages=['django_gcs'],
+    include_package_data=True,
+    license='MIT License',
+    description='Google Cloud Storage for Django',
+    long_description=README,
+    author='Colin Su<littleq0903@gmail.com>, Gerasev Kirill',
+    author_email='gerasev.kirill@gmail.com',
+    install_requires=[
+        'Django>=1.10',
+        'google-cloud==0.22.0'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
